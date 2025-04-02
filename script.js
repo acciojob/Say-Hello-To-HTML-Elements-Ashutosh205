e750e79b9085818a332b96aca70a85be94f3bf08
Ashutosh solanki <113789261+Ashutosh205@users.noreply.github.com>
2025-04-02 19:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Creating and appending h1
+    let h1 = document.createElement("h1");
+    h1.textContent = "Hello World";
+    document.body.appendChild(h1);
+
+    // Creating and appending h2
+    let h2 = document.createElement("h2");
+    h2.textContent = "Introducing the Accio Ipsum";
+    document.body.appendChild(h2);
+
+    // Creating and appending paragraph
+    let p = document.createElement("p");
+    p.textContent = "Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.";
+    document.body.appendChild(p);
+});
